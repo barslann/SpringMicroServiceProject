7df2882b56fefb039b764ced5870182d9475c8e0
coderman <barslann58@gmail.com>
2021-08-01 22:11:51 -0700

    some microservices added

---
+++ PhotoAppApiUsers/src/main/java/com/coderman/photoapp/api/users/photoappapiusers/ui/controllers/UserController.java
@@ -0,0 +1,21 @@
+package com.coderman.photoapp.api.users.photoappapiusers.ui.controllers;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    Environment environment;
+
+    @GetMapping("/status/check")
+    public String status(){
+        return "Working" + environment.getProperty("local.server.port");
+    }
+}
